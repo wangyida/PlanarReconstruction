b91757baa4845560685a6a2dc5efc00f7044c279
yanyan-li <yanyan.li@pku.edu.cn>
2019-12-06 16:47:30 +0100

    points

--- main.py
+++ main.py
@@ -542,8 +542,8 @@ def eval(_run, _log):
             # save some point clouds
             Camera_fx = 517.97
             Camera_fy = 517.97
-            Camera_cx = 320
-            Camera_cy = 240
+            Camera_cx = 128
+            Camera_cy = 96
             points = []
             points_instance=[]
             scalingFactor = 1.0
@@ -555,10 +555,10 @@ def eval(_run, _log):
                     label_instance=predict_segmentation[v, u]
                     Z = (depth[v, u]/scalingFactor)[0]
                     if Z == 0: continue
-                    # X = (u - Camera_cx/2) * Z / Camera_fx*2
-                    # Y = (v - Camera_cy/2) * Z / Camera_fy*2
-                    X = (u - Camera_cx) * Z / Camera_fx
-                    Y = (v - Camera_cy) * Z / Camera_fy
+                    X = (u - Camera_cx/2) * Z / Camera_fx*2
+                    Y = (v - Camera_cy/2) * Z / Camera_fy*2
+                    # X = (u - Camera_cx) * Z / Camera_fx
+                    # Y = (v - Camera_cy) * Z / Camera_fy
                     points_feat = np.concatenate((points_feat, [X,Z,Y,color_instance[0],color_instance[1],color_instance[2]]), axis = 0)
             points_feat = points_feat.reshape((-1,6))
             save_pcd('./pointCloud_instance.ply', points_feat)
